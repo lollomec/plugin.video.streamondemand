07f63e29681450a2ae71d8e876945758569615a2
Dentaku65 <dentaku65@gmail.com>
2017-07-04 00:22:46 +0200

    new channel added

---
+++ channels/serietvonline.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# streamondemand.- XBMC Plugin
+# Canale per serietvonline
+# http://www.mimediacenter.info/foro/viewforum.php?f=36
+# ------------------------------------------------------------
+import re
+import urlparse
+
+from core import config, httptools
+from core import logger
+from core import scrapertools
+from core import servertools
+from core.item import Item
+from core.tmdb import infoSod
+
+__channel__ = "serietvonline"
+
+host = "https://serietvonline.com"
+
+headers = [['Referer', host]]
+
+
+def mainlist(item):
+    logger.info("streamondemand.serietvonline mainlist")
+    itemlist = [Item(channel=__channel__,
+                     title="[COLOR azure]Aggiornamenti[/COLOR]",
+                     action="serietv",
+                     extra='serie',
+                     url=host,
+                     thumbnail="http://orig03.deviantart.net/6889/f/2014/079/7/b/movies_and_popcorn_folder_icon_by_matheusgrilo-d7ay4tw.png"),
+                Item(channel=__channel__,
+                     title="[COLOR yellow]Cerca...[/COLOR]",
+                     action="search",
+                     extra='serie',
+                     thumbnail="http://dc467.4shared.com/img/fEbJqOum/s7/13feaf0c8c0/Search")]
+
+    return itemlist
+
+
+def serietv(item):
+    logger.info("streamondemand.serietvonline peliculas")
+    itemlist = []
+
+    # Descarga la pagina
+    data = httptools.downloadpage(item.url, headers=headers).data
+
+    # Extrae las entradas (carpetas)
+    patron = '<a href="([^"]+)"><span[^>]+><[^>]+><\/a>[^h]+h2>(.*?)<'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+
+    for scrapedurl, scrapedtitle in matches:
+        scrapedplot = ""
+        scrapedthumbnail = ""
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        itemlist.append(infoSod(
+            Item(channel=__channel__,
+                 action="episodios",
+                 fulltitle=scrapedtitle,
+                 show=scrapedtitle,
+                 title=scrapedtitle,
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail,
+                 plot=scrapedplot,
+                 extra=item.extra,
+                 folder=True), tipo='tv'))
+
+    # Extrae el paginador
+    patronvideos = '<div class="siguiente"><a href="([^"]+)">'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
+
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="HomePage",
+                 title="[COLOR yellow]Torna Home[/COLOR]",
+                 folder=True)),
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="serietv",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="http://2.bp.blogspot.com/-fE9tzwmjaeQ/UcM2apxDtjI/AAAAAAAAeeg/WKSGM2TADLM/s1600/pager+old.png",
+                 extra=item.extra,
+                 folder=True))
+
+    return itemlist
+
+def episodios(item):
+    itemlist = []
+
+    # Descarga la pagina
+    data = httptools.downloadpage(item.url, headers=headers).data
+    bloque = scrapertools.get_match(data, '<table>(.*?)</table>')
+
+    # Extrae las entradas (carpetas)
+    patron = '<tr><td>(.*?)<[^h]+href=\'(.*?)\' target=\'_blank\'>'
+    matches = re.compile(patron, re.DOTALL).findall(bloque)
+
+    for scrapedtitle, scrapedurl in matches:
+        scrapedplot = ""
+        scrapedthumbnail = ""
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="findvideos",
+                 fulltitle=scrapedtitle,
+                 show=scrapedtitle,
+                 title="[COLOR azure]" + scrapedtitle + "[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail=scrapedthumbnail,
+                 plot=scrapedplot,
+                 folder=True))
+
+    if config.get_library_support() and len(itemlist) != 0:
+        itemlist.append(
+            Item(channel=__channel__,
+                 title="Aggiungi alla libreria",
+                 url=item.url,
+                 action="add_serie_to_library",
+                 extra="episodios",
+                 show=item.show))
+
+    return itemlist
+
+def findvideos(item):
+    logger.info("[serietvonline.py] findvideos")
+
+    # Descarga la página
+    if 'vcrypt' in item.url:
+        item.url = httptools.downloadpage(item.url, only_headers=True, follow_redirects=False).headers.get("location")
+        data = item.url
+
+    else:
+        data = item.url
+
+    itemlist = servertools.find_video_items(data=data)
+
+    for videoitem in itemlist:
+        videoitem.title = item.show
+        videoitem.fulltitle = item.fulltitle
+        videoitem.show = item.show
+        videoitem.thumbnail = item.thumbnail
+        videoitem.channel = __channel__
+
+    return itemlist
+
+def HomePage(item):
+    import xbmc
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
